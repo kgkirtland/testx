6a3a6069966bad4b80bc342742aee131ef4a84f9
kgkirtland <kgkirtland@hotmail.com>
2019-01-25 13:23:34 -0500

    updated jasmine testing

---
+++ js/spec/main.js
@@ -0,0 +1,28 @@
+var m = require('../src/test');
+
+describe('Add test', function() {
+  it('2 + 3 = 5', function() {
+    let x = 2;
+    let y = 3;
+    let expected = 5;
+    let actual;
+
+    actual = m.add(x, y);
+
+    expect(expected).toEqual(actual);
+
+  });
+  
+  it('8 + 48 = 56', function() {
+    let x = 8;
+    let y = 48;
+    let expected = 56;
+    let actual;
+
+    actual = m.add(x, y);
+
+    expect(expected).toEqual(actual);
+
+  });
+});
+
--- js/src/test.js
+++ js/src/test.js
@@ -1,19 +1,8 @@
-var main = (function() {
-    var add = function add(x, y)
-    {
-        return x + y;
-    };
+exports.add = function add(x, y)
+{
+    return x + y;
+};
 
-    var helloWorld = function helloWorld() {
-        return 'Hello world!';
-    };
-
-    return {
-        add: add,
-        helloWorld: helloWorld
-    }
-})();
-
-main.add(3,6);
-
-module.exports = main;
+exports.helloWorld = function helloWorld() {
+    return 'exportsHello world!';
+};
